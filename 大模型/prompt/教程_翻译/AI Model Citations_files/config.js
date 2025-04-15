c6dbc41058903b0997286d93dbcdb1bfef71c5a4
jiang.li <1298334835@qq.com>
2025-04-15 10:07:47 +0800

    vault backup: 2025-04-15 10:07:47

---
+++ 大模型/prompt/教程_翻译/AI Model Citations_files/config.js
@@ -0,0 +1,7 @@
+(function() {
+  window._POSTHOG_REMOTE_CONFIG = window._POSTHOG_REMOTE_CONFIG || {};
+  window._POSTHOG_REMOTE_CONFIG['phc_QH92qoJ9zYh2LUuj5goa6NgqgnMOAkdR6gqUCEJL1Eq'] = {
+    config: {"token": "phc_QH92qoJ9zYh2LUuj5goa6NgqgnMOAkdR6gqUCEJL1Eq", "supportedCompression": ["gzip", "gzip-js"], "hasFeatureFlags": false, "captureDeadClicks": false, "capturePerformance": {"network_timing": true, "web_vitals": true, "web_vitals_allowed_metrics": null}, "autocapture_opt_out": false, "autocaptureExceptions": false, "analytics": {"endpoint": "/i/v0/e/"}, "elementsChainAsString": true, "sessionRecording": false, "quotaLimited": ["recordings"], "heatmaps": true, "surveys": false, "defaultIdentifiedOnly": true},
+    siteApps: []
+  }
+})();
